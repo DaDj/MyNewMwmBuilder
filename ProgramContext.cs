eee9d0ed8f917ba3edf26e6418bc162a7b89d7c7
Patrick Gramann <patgramann@gmail.com>
2021-10-13 22:46:06 +0200

    Materialspath argument

--- ProgramContext.cs
+++ ProgramContext.cs
@@ -68,6 +68,8 @@ namespace MwmBuilder
             m_args.RegisterArg("d", "LODS", null, "Float values separated by space, defining default values for LOD 1-n");
             m_args.RegisterArg("do", "LODSOverride", null, "Float values separated by space, defining overriden values for LOD 1-n");
             m_args.RegisterArg("gss", "SHAREDSTUB", true.ToString(), "Generate shared stub and save LOD0 with postfix _LOD0");
+            m_args.RegisterArg("x", "MATERIALSLIB", @"E:\Steam\SteamApps\common\SpaceEngineersModSDK\OriginalContent\Materials", "Path to materials library"); //Materials Path argument
+
             string[] args = (string[])data;
             try
             {
@@ -106,7 +108,7 @@ namespace MwmBuilder
                     }
                 }
                 string[] strArray = LoadFiles(m_args.GetValue("s"), m_args.GetValue("m"));
-                setMaterialsPathForSource(m_args.GetValue("s"));
+                setMaterialsPathForSource(m_args.GetValue("x")); //Materials Path argument
                 ProgramContext.LoadMaterialLibs();
                 m_count = strArray.Length;
                 if (m_args.GetValue("t") != null)
@@ -136,24 +138,31 @@ namespace MwmBuilder
 
         private void setMaterialsPathForSource(string sourcePath)
         {
+            //if (sourcePath == null)
+            //    return;
+            //string[] strArray = sourcePath.Split(';');
+            //DirectoryInfo directoryInfo = null;
+            //if (strArray.Length >= 1)
+            //    directoryInfo = Directory.GetParent(strArray[0]);
+            //for (; directoryInfo != null; directoryInfo = directoryInfo.Parent)
+            //{
+            //    foreach (DirectoryInfo directory in directoryInfo.GetDirectories())
+            //    {
+            //        if (directory.Name == "Materials")
+            //        {
+            //            ProgramContext.m_materialsPath = directory.FullName;
+            //            return;
+            //        }
+            //    }
+            //}
+            //ProgramContext.m_logger.LogMessage(MessageType.Error, "Could not find Materials library folder.", "");
             if (sourcePath == null)
                 return;
-            string[] strArray = sourcePath.Split(';');
-            DirectoryInfo directoryInfo = null;
-            if (strArray.Length >= 1)
-                directoryInfo = Directory.GetParent(strArray[0]);
-            for (; directoryInfo != null; directoryInfo = directoryInfo.Parent)
-            {
-                foreach (DirectoryInfo directory in directoryInfo.GetDirectories())
-                {
-                    if (directory.Name == "Materials")
-                    {
-                        ProgramContext.m_materialsPath = directory.FullName;
-                        return;
-                    }
-                }
-            }
-            ProgramContext.m_logger.LogMessage(MessageType.Error, "Could not find Materials library folder.", "");
+
+            if (!Directory.Exists(Path.GetFullPath(sourcePath)))
+                ProgramContext.m_logger.LogMessage(MessageType.Warning, "Could not find Materials library folder.", sourcePath);
+            else
+                ProgramContext.m_materialsPath = Path.GetFullPath(sourcePath);
         }
 
         private void ProcessFileSafe(string file)
